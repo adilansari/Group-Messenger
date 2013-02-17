36bf73b7b4da4734030cfe16e45031a31d356092
adil <mansari@buffalo.edu>
2013-02-17 04:30:19 -0500

    Content Provider implemented. insert() tested working, query() pending

--- src/edu/buffalo/cse/cse486586/groupmessenger/myHelper.java
+++ src/edu/buffalo/cse/cse486586/groupmessenger/myHelper.java
@@ -1,31 +1,33 @@
 package edu.buffalo.cse.cse486586.groupmessenger;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class myHelper extends SQLiteOpenHelper {
 
 	
-	public static final String DB_NAME = "DataBase";
+	public static final String DB_NAME = "KeyValueStore.db";
 	public static final int DB_VERSION= 1;
-	public static final String TABLE_NAME= "table";
-	public static final String key= "key";
-	public static final String value = "value";
-	public static final String CREATE_TABLE= "create table " + TABLE_NAME + "(" + key + " integer primary key autoincrement, " + value + " text not null);";
-	
+	public static final String TABLE_NAME= "Store";
+	public static final String KEY_FIELD= "key";
+	public static final String VALUE_FIELD = "value";
+	public static final String CREATE_TABLE= "CREATE TABLE " + TABLE_NAME + "(" + KEY_FIELD + " TEXT PRIMARY KEY NOT NULL, " + VALUE_FIELD + " TEXT);";
+	public static final String TAG= "adil";
 	
 	public myHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
-		arg0.execSQL(CREATE_TABLE);		
-	}
+		Log.v(TAG, "onCreate call");			
+		arg0.execSQL(CREATE_TABLE);
+		Log.v(TAG, "Table created");
+		}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
@@ -34,5 +36,9 @@ public class myHelper extends SQLiteOpenHelper {
 		 onCreate(arg0);
 	}
 
-	
+	public void insertPair(String key, String value) {
+		ContentValues keyValuesToInsert= new ContentValues();
+		keyValuesToInsert.put(myHelper.KEY_FIELD, key);
+		keyValuesToInsert.put(myHelper.VALUE_FIELD, value);
+	}
 }
--- src/edu/buffalo/cse/cse486586/groupmessenger/myProvider.java
+++ src/edu/buffalo/cse/cse486586/groupmessenger/myProvider.java
@@ -1,5 +1,79 @@
 package edu.buffalo.cse.cse486586.groupmessenger;
 
-public class myProvider {
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.util.Log;
+
+public class myProvider extends ContentProvider {
+
+	private myHelper myDb;
+	private SQLiteDatabase db;
+	private static final String AUTHORITY = "edu.buffalo.cse.cse486586.groupmessenger.provider";
+	private static final String BASE_PATH = myHelper.TABLE_NAME;
+	public static final Uri CONTENT_URI = Uri.parse("content://"+ AUTHORITY + "/" + BASE_PATH);
+	public static final String TAG= "adil";
+	
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri arg0, ContentValues arg1) {
+		db = myDb.getWritableDatabase();
+		ContentValues values= new ContentValues(arg1);
+		long rowId= db.insert(myHelper.TABLE_NAME, myHelper.VALUE_FIELD, values);
+		
+		if (rowId > 0) {
+			Uri newUri = ContentUris.withAppendedId(CONTENT_URI, rowId);
+			getContext().getContentResolver().notifyChange(newUri, null);
+			Log.e(TAG, "Insertion success!!");
+			return newUri;
+		}
+		else {
+			Log.e(TAG, "Insert to db failed");
+		}
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		Log.v(TAG, "provider created");
+		myDb = new myHelper(getContext());
+		myDb.getWritableDatabase();
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri arg0, String[] arg1, String arg2, String[] arg3,String arg4) {
+		db= myDb.getWritableDatabase();
+		SQLiteQueryBuilder qb= new SQLiteQueryBuilder();
+		qb.setTables(myHelper.TABLE_NAME);
+		
+		qb.appendWhereEscapeString(myHelper.KEY_FIELD + " = " + arg2);
+		Cursor c= qb.query(db, null, null, null, null, null, null);
+		//Cursor c= db.query(myHelper.TABLE_NAME, null, arg2, null, null, null, null);
+		c.setNotificationUri(getContext().getContentResolver(), arg0);
+		return c;
+	}
+
+	@Override
+	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
